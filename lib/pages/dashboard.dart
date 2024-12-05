a07e1fec409edcc39bc626d1e6117d3df869ac88
EVA THOMAS <168407738+evathomas04@users.noreply.github.com>
2024-12-05 22:39:53 +0530

    dashboard.dart

--- lib/pages/dashboard.dart
+++ lib/pages/dashboard.dart
@@ -1,11 +1,27 @@
 import 'package:flutter/material.dart';
 
 class DashboardPage extends StatelessWidget {
+  const DashboardPage({super.key});
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('Dashboard')),
-      body: const Center(child: Text('Dashboard Page content')),
+      appBar: AppBar(
+        leading: IconButton(
+          icon: const Icon(Icons.menu),
+          onPressed: () {
+            Scaffold.of(context).openDrawer(); // Opens the drawer
+          },
+        ),
+        title: Center(
+          child: Image.asset(
+            'assets/logo.png', // Your logo path here
+            fit: BoxFit.contain,
+            height: 40,
+          ),
+        ),
+      ),
+      body: const Center(child: Text('Dash Page content')),
     );
   }
 }
