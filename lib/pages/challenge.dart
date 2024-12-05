19d7c5a4ca0cacb6b077126b862e16d491bdc522
EVA THOMAS <168407738+evathomas04@users.noreply.github.com>
2024-12-05 22:38:57 +0530

    challenge.dart

--- lib/pages/challenge.dart
+++ lib/pages/challenge.dart
@@ -1,11 +1,27 @@
 import 'package:flutter/material.dart';
 
 class ChallengePage extends StatelessWidget {
+  const ChallengePage({super.key});
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('Challenge')),
-      body: const Center(child: Text('Challenge Page Content')),
+      appBar: AppBar(
+        leading: IconButton(
+          icon: const Icon(Icons.menu),
+          onPressed: () {
+            Scaffold.of(context).openDrawer(); // Opens the drawer
+          },
+        ),
+        title: Center(
+          child: Image.asset(
+            'assets/logo.png', // Your logo path here
+            fit: BoxFit.contain,
+            height: 40,
+          ),
+        ),
+      ),
+      body: const Center(child: Text('Challenge Page content')),
     );
   }
 }
