60124db33604c96bd95ecac2718a50db1ed1f19b
EVA THOMAS <168407738+evathomas04@users.noreply.github.com>
2024-12-05 22:39:24 +0530

    contact.dart

--- lib/pages/contact.dart
+++ lib/pages/contact.dart
@@ -1,11 +1,27 @@
 import 'package:flutter/material.dart';
 
 class ContactPage extends StatelessWidget {
+  const ContactPage({super.key});
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: const Text('Contact')),
-      body: const Center(child: Text('Contact Page Content')),
+      appBar: AppBar(
+        leading: IconButton(
+          icon: const Icon(Icons.menu),
+          onPressed: () {
+            Scaffold.of(context).openDrawer(); // Opens the drawer
+          },
+        ),
+        title: Center(
+          child: Image.asset(
+            'assets/logo.png', // Your logo path here
+            fit: BoxFit.contain,
+            height: 40,
+          ),
+        ),
+      ),
+      body: const Center(child: Text('Contact Page content')),
     );
   }
 }
